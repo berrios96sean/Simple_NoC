cbab02a4a201983c719e1094b169b51eddb491b7
Sean Berrios <berrios96sean@gmail.com>
2024-06-09 20:21:00 -0700

    num gen flow control fix

--- src/num_generator/num_gen.sv
+++ src/num_generator/num_gen.sv
@@ -69,13 +69,11 @@ module num_gen #(
                 o_data <= g_data;
                 o_dest <= 2'b01;
 
-                AXIS_S_TREADY <= 1'b1;
                 AXIS_M_TVALID <= 1'b1;
                 AXIS_M_TLAST  <= 1'b1;
 
             end else begin
 
-                AXIS_S_TREADY <= 1'b0;
                 AXIS_M_TVALID <= 1'b0;
                 AXIS_M_TLAST  <= 1'b0;
 
