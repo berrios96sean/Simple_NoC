4901bdd81f55c22401083a9d197e7cc7af25d488
berrios96sean <berrios96sean@gmail.com>
2024-06-20 16:50:02 -0700

    adding io assignments

---
+++ build/io_constraints.tcl
@@ -0,0 +1,122 @@
+# Copyright (C) 2023  Intel Corporation. All rights reserved.
+# Your use of Intel Corporation's design tools, logic functions 
+# and other software and tools, and any partner logic 
+# functions, and any output files from any of the foregoing 
+# (including device programming or simulation files), and any 
+# associated documentation or information are expressly subject 
+# to the terms and conditions of the Intel Program License 
+# Subscription Agreement, the Intel Quartus Prime License Agreement,
+# the Intel FPGA IP License Agreement, or other applicable license
+# agreement, including, without limitation, that your use is for
+# the sole purpose of programming logic devices manufactured by
+# Intel and sold by Intel or its authorized distributors.  Please
+# refer to the Intel FPGA Software License Subscription Agreements 
+# on the Quartus Prime software download page.
+
+# Quartus Prime Version 23.4.0 Build 79 11/22/2023 SC Pro Edition
+# File: /mnt/vault0/sfberrio/repos/Simple_NoC/build/io_constraints.tcl
+# Generated on: Thu Jun 20 16:49:30 2024
+
+package require ::quartus::project
+
+set_location_assignment IOBANK_2A -to RST_N
+set_location_assignment IOBANK_2A -to START
+set_location_assignment IOBANK_2A -to DONE
+set_location_assignment IOBANK_2A -to ODATA_O[7]
+set_location_assignment IOBANK_2A -to IDATA_O2[12]
+set_location_assignment IOBANK_2A -to IDATA_O1[17]
+set_location_assignment IOBANK_2A -to ODATA_O[6]
+set_location_assignment IOBANK_2A -to IDATA_O2[11]
+set_location_assignment IOBANK_2A -to IDATA_O1[16]
+set_location_assignment IOBANK_2A -to ODATA_O[5]
+set_location_assignment IOBANK_2A -to IDATA_O2[10]
+set_location_assignment IOBANK_2A -to IDATA_O1[15]
+set_location_assignment IOBANK_2A -to ODATA_O[31]
+set_location_assignment IOBANK_2A -to ODATA_O[30]
+set_location_assignment IOBANK_2A -to ODATA_O[29]
+set_location_assignment IOBANK_2A -to ODATA_O[28]
+set_location_assignment IOBANK_2A -to ODATA_O[27]
+set_location_assignment IOBANK_2A -to ODATA_O[26]
+set_location_assignment IOBANK_2A -to ODATA_O[25]
+set_location_assignment IOBANK_2A -to ODATA_O[24]
+set_location_assignment IOBANK_2A -to ODATA_O[23]
+set_location_assignment IOBANK_2A -to ODATA_O[22]
+set_location_assignment IOBANK_2A -to ODATA_O[21]
+set_location_assignment IOBANK_2A -to ODATA_O[20]
+set_location_assignment IOBANK_2A -to ODATA_O[19]
+set_location_assignment IOBANK_2A -to ODATA_O[18]
+set_location_assignment IOBANK_2A -to ODATA_O[17]
+set_location_assignment IOBANK_2A -to ODATA_O[16]
+set_location_assignment IOBANK_2A -to ODATA_O[15]
+set_location_assignment IOBANK_2A -to ODATA_O[14]
+set_location_assignment IOBANK_2A -to ODATA_O[13]
+set_location_assignment IOBANK_2A -to ODATA_O[12]
+set_location_assignment IOBANK_2A -to ODATA_O[11]
+set_location_assignment IOBANK_2A -to ODATA_O[10]
+set_location_assignment IOBANK_2A -to ODATA_O[9]
+set_location_assignment IOBANK_2A -to ODATA_O[8]
+set_location_assignment IOBANK_2A -to ODATA_O[4]
+set_location_assignment IOBANK_2A -to ODATA_O[3]
+set_location_assignment IOBANK_2A -to ODATA_O[2]
+set_location_assignment IOBANK_2A -to ODATA_O[1]
+set_location_assignment IOBANK_2A -to ODATA_O[0]
+set_location_assignment IOBANK_2A -to IDATA_O2[9]
+set_location_assignment IOBANK_2A -to IDATA_O1[14]
+set_location_assignment IOBANK_2A -to IDATA_O2[8]
+set_location_assignment IOBANK_2A -to IDATA_O1[13]
+set_location_assignment IOBANK_2A -to IDATA_O2[7]
+set_location_assignment IOBANK_2A -to IDATA_O1[12]
+set_location_assignment IOBANK_2A -to IDATA_O2[6]
+set_location_assignment IOBANK_2A -to IDATA_O1[11]
+set_location_assignment IOBANK_2A -to IDATA_O2[5]
+set_location_assignment IOBANK_2A -to IDATA_O1[10]
+set_location_assignment IOBANK_2A -to IDATA_O2[31]
+set_location_assignment IOBANK_2A -to IDATA_O2[30]
+set_location_assignment IOBANK_2A -to IDATA_O2[29]
+set_location_assignment IOBANK_2A -to IDATA_O2[28]
+set_location_assignment IOBANK_2A -to IDATA_O2[27]
+set_location_assignment IOBANK_2A -to IDATA_O2[26]
+set_location_assignment IOBANK_2A -to IDATA_O2[25]
+set_location_assignment IOBANK_2A -to IDATA_O2[24]
+set_location_assignment IOBANK_2A -to IDATA_O2[23]
+set_location_assignment IOBANK_2A -to IDATA_O2[22]
+set_location_assignment IOBANK_2A -to IDATA_O2[21]
+set_location_assignment IOBANK_2A -to IDATA_O2[20]
+set_location_assignment IOBANK_2A -to IDATA_O2[19]
+set_location_assignment IOBANK_2A -to IDATA_O2[18]
+set_location_assignment IOBANK_2A -to IDATA_O2[17]
+set_location_assignment IOBANK_2A -to IDATA_O2[16]
+set_location_assignment IOBANK_2A -to IDATA_O2[15]
+set_location_assignment IOBANK_2A -to IDATA_O2[14]
+set_location_assignment IOBANK_2A -to IDATA_O2[13]
+set_location_assignment IOBANK_2A -to IDATA_O2[4]
+set_location_assignment IOBANK_2A -to IDATA_O2[3]
+set_location_assignment IOBANK_2A -to IDATA_O2[2]
+set_location_assignment IOBANK_2A -to IDATA_O2[1]
+set_location_assignment IOBANK_2A -to IDATA_O2[0]
+set_location_assignment IOBANK_2A -to IDATA_O1[9]
+set_location_assignment IOBANK_2A -to IDATA_O1[8]
+set_location_assignment IOBANK_2A -to IDATA_O1[7]
+set_location_assignment IOBANK_2A -to IDATA_O1[6]
+set_location_assignment IOBANK_2A -to IDATA_O1[5]
+set_location_assignment IOBANK_2A -to IDATA_O1[31]
+set_location_assignment IOBANK_2A -to IDATA_O1[30]
+set_location_assignment IOBANK_2A -to IDATA_O1[29]
+set_location_assignment IOBANK_2A -to IDATA_O1[28]
+set_location_assignment IOBANK_2A -to IDATA_O1[27]
+set_location_assignment IOBANK_2A -to IDATA_O1[26]
+set_location_assignment IOBANK_2A -to IDATA_O1[25]
+set_location_assignment IOBANK_2A -to IDATA_O1[24]
+set_location_assignment IOBANK_2A -to IDATA_O1[23]
+set_location_assignment IOBANK_2A -to IDATA_O1[22]
+set_location_assignment IOBANK_2A -to IDATA_O1[21]
+set_location_assignment IOBANK_2A -to IDATA_O1[20]
+set_location_assignment IOBANK_2A -to IDATA_O1[19]
+set_location_assignment IOBANK_2A -to IDATA_O1[18]
+set_location_assignment IOBANK_2A -to IDATA_O1[4]
+set_location_assignment IOBANK_2A -to IDATA_O1[3]
+set_location_assignment IOBANK_2A -to IDATA_O1[2]
+set_location_assignment IOBANK_2A -to IDATA_O1[1]
+set_location_assignment IOBANK_2A -to IDATA_O1[0]
+set_location_assignment IOBANK_2A -to CLK
+set_location_assignment IOBANK_2A -to CLK_NOC
