8f95c00d509f2e1ce94f642b5d873efa22008bf4
Sean Berrios <berrios96sean@gmail.com>
2024-06-20 16:27:48 -0700

    fixing constraints import

--- build/import_constraints.tcl
+++ build/import_constraints.tcl
@@ -1,10 +1,12 @@
 exec mkdir -p constraints
 
-file copy -force ../../src/noc/dcfifo_agilex7.sv                     constraints/dcfifo_agilex7.sv
-file copy -force ../../src/noc/dcfifo_mixed_width_agilex7.sv         constraints/dcfifo_mixed_width_agilex7.sv
-file copy -force ../../src/noc/reset_synchronizer.sv                 constraints/reset_synchronizer.sv
+file copy -force ../../src/noc/dcfifo_agilex7.sdc                     constraints/dcfifo_agilex7.sdc
+file copy -force ../../src/noc/dcfifo_mixed_width_agilex7.sdc         constraints/dcfifo_mixed_width_agilex7.sdc
+file copy -force ../../src/noc/reset_synchronizer.sdc                 constraints/reset_synchronizer.sdc
+file copy -force ../../src/top/noc_adder.sdc                          constraints/noc_adder.sdc
 
 
-set_global_assignment -name SDC_FILE  constraints/dcfifo_agilex7.sv
-set_global_assignment -name SDC_FILE  constraints/dcfifo_mixed_width_agilex7.sv
-set_global_assignment -name SDC_FILE  constraints/reset_synchronizer.sv
+set_global_assignment -name SDC_FILE  constraints/dcfifo_agilex7.sdc
+set_global_assignment -name SDC_FILE  constraints/dcfifo_mixed_width_agilex7.sdc
+set_global_assignment -name SDC_FILE  constraints/reset_synchronizer.sdc
+set_global_assignment -name SDC_FILE  constraints/noc_adder.sdc
