bf9e679d0240aa79e178210159bc1a20e10c169e
berrios96sean <berrios96sean@gmail.com>
2024-06-28 14:35:47 -0700

    adding tcl script for running mvm tb

---
+++ sim/mvm_tb.tcl
@@ -0,0 +1,57 @@
+----------------------------------------
+# TOP-LEVEL TEMPLATE - BEGIN
+#
+# QSYS_SIMDIR is used in the Quartus-generated IP simulation script to
+# construct paths to the files required to simulate the IP in your Quartus
+# project. By default, the IP script assumes that you are launching the
+# simulator from the IP script location. If launching from another
+# location, set QSYS_SIMDIR to the output directory you specified when you
+# generated the IP script, relative to the directory from which you launch
+# the simulator.
+#
+#set QSYS_SIMDIR /mnt/vault0/sfberrio/repos/Simple_NoC/sim
+set QSYS_SIMDIR [pwd]
+#
+#
+# Source the generated IP simulation script.
+# Script may fail if modelsim.ini file is not in read only mode
+# adjust by using chmod 444 <dir>/modelsim.ini
+# source $QSYS_SIMDIR/mentor/msim_setup.tcl
+#
+# Set any compilation options you require (this is unusual).
+# set USER_DEFINED_COMPILE_OPTIONS <compilation options>
+# set USER_DEFINED_VHDL_COMPILE_OPTIONS <compilation options for VHDL>
+# set USER_DEFINED_VERILOG_COMPILE_OPTIONS <compilation options for Verilog>
+#
+# Call command to compile the Quartus EDA simulation library.
+# dev_com
+#
+# Call command to compile the Quartus-generated IP simulation files.
+# com
+#
+# Add commands to compile all design files and testbench files, including
+# the top level. (These are all the files required for simulation other
+# than the files compiled by the Quartus-generated IP simulation script)
+#
+vlog +acc $QSYS_SIMDIR/testbench/mvm_tb.v \
+    $QSYS_SIMDIR/../src/mvm/*v 
+#
+# Set the top-level simulation or testbench module/entity name, which is
+# used by the elab command to elaborate the top level.
+#
+set TOP_LEVEL_NAME mvm_tb
+#
+# Set any elaboration options you require.
+# set USER_DEFINED_ELAB_OPTIONS <elaboration options>
+#
+# Call command to elaborate your design and testbench.
+elab_debug
+#
+# do wave.do
+# Run the simulation.
+run -a
+#
+# Report success to the shell.
+# exit -code 0
+#
+# TOP-LEVEL TEMPLATE - END
